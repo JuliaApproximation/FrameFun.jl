3c29b9130217d7c7242407f25401507bea0533b6
Daan Huybrechs <daan.huybrechs@cs.kuleuven.be>
2015-11-23 09:43:40 +0100

    Deleted my_lsqr routine

--- src/fe_fourier.jl
+++ src/fe_fourier.jl
@@ -1,68 +1,6 @@
 # fe_fourier.jl
 
 
-
-# Perform the copy without additional allocation
-@generated function copy_ranges!{N}(c, d, c_ranges::NTuple{N}, d_ranges::NTuple{N})
-    quote
-        @nloops $N i x->1:length(c_ranges[x]) begin
-         (@nref $N d x->d_ranges[x][i_x]) = (@nref $N c x->c_ranges[x][i_x])
-        end
-    end
-end
-
-
-## This code is in anticipation of a working TensorProductOperator implementation.
-# TODO: fix Chebyshev transform and remove these functions below
-
-apply_tensor!(op::Extension, dest::TensorProductSet, src::TensorProductSet, dest1::ChebyshevBasis, src1::ChebyshevBasis, coef_dest, coef_src) = 
-    reshape_N_to_L_cheby!(coef_src, coef_dest, size(coef_src), size(coef_dest))
-
-apply_tensor!(op::Restriction, dest::TensorProductSet, src::TensorProductSet, dest1::ChebyshevBasis, src1::ChebyshevBasis, coef_dest, coef_src) =
-    reshape_L_to_N_cheby!(coef_dest, coef_src, size(coef_dest), size(coef_src))
-
-# Reshape functions: we want to efficiently copy the data from a vector of length N to a larger vector of length L.
-# Hard to do with Cartesian, but it can be done for any dimension recursively.
-# TODO: find a better way!
-function reshape_N_to_L_cheby!{N}(c, d, n::NTuple{N}, l::NTuple{N})
-    nh = map(x->div(x-1,2), n)
-    # First zero out d
-    fill!(d, 0)
-    reshape_N_to_L_rec_cheby!(c, d, (), (), nh, n, l)
-end
-
-function reshape_N_to_L_rec_cheby!{N}(c, d, c_ranges, d_ranges, nh::NTuple{N}, n, l)
-    reshape_N_to_L_rec_cheby!(c, d, tuple(c_ranges...,1:n[1]), tuple(d_ranges...,1:n[1]), nh[2:end], n[2:end], l[2:end])
-end
-
-# The end of the recursion: perform the actual copy
-function reshape_N_to_L_rec_cheby!(c, d, c_ranges, d_ranges, nh::NTuple{1}, n::NTuple{1}, l::NTuple{1})
-    # Currently, the two lines below do some allocation. Using views is not a great improvement.
-    # d[d_ranges...,1:nh[1]+1] = c[c_ranges...,1:nh[1]+1]
-    # d[d_ranges...,l[1]-nh[1]+1:l[1]] = c[c_ranges...,n[1]-nh[1]+1:n[1]]
-    copy_ranges!(c, d, tuple(c_ranges...,1:n[1]), tuple(d_ranges...,1:n[1]))
-end
-
-
-function reshape_L_to_N_cheby!{N}(c, d, n::NTuple{N}, l::NTuple{N})
-    nh = map(x->div(x-1, 2), n)
-    reshape_L_to_N_rec_cheby!(c, d, (), (), nh, n, l)
-end
-
-function reshape_L_to_N_rec_cheby!{N}(c, d, c_ranges, d_ranges, nh::NTuple{N}, n, l)
-    reshape_L_to_N_rec_cheby!(c, d, tuple(c_ranges...,1:n[1]), tuple(d_ranges...,1:n[1]), nh[2:end], n[2:end], l[2:end])
-end
-
-# The end of the recursion: perform the actual copy
-function reshape_L_to_N_rec_cheby!(c, d, c_ranges, d_ranges, nh::NTuple{1}, n::NTuple{1}, l::NTuple{1})
-    copy_ranges!(d, c, tuple(d_ranges...,1:n[1]), tuple(c_ranges...,1:n[1]))
-end
-
-
-
-
-
-
 function apply!{T,G <: MaskedGrid}(op::Extension, dest, src::DiscreteGridSpace{G}, coef_dest::Array{T}, coef_src::Array{T})
     @assert length(coef_src) == length(src)
     @assert length(coef_dest) == length(dest)
