bdefaa7394d7af8a9e12c96b2ce2ccf6395bc54a
Daan Huybrechs <daan.huybrechs@cs.kuleuven.be>
2017-01-17 13:08:17 +0100

    Remove complexify, clean up sum frames to use TruncatedSVDSolver

--- src/approximation/lowranksolver.jl
+++ src/approximation/lowranksolver.jl
@@ -1,5 +1,4 @@
-
-# fastsolver.jl
+# lowranksolver.jl
 
 """
 A Randomized Truncated SVD solver storing a decomposition of an operator A.
@@ -18,34 +17,40 @@ immutable TruncatedSvdSolver{ELT} <: AbstractOperator{ELT}
     # For storing intermediate results when applying
     y           ::  Array{ELT,1}
     sy          ::  Array{ELT,1}
-    function TruncatedSvdSolver(op::AbstractOperator; cutoff = default_cutoff(problem), R = 5, verbose = false, options...)
+    scratch_src ::  Array{ELT,1}
+    scratch_dest::  Array{ELT,1}
+
+    function TruncatedSvdSolver(op::AbstractOperator; cutoff = default_cutoff(problem), R = 5, growth_factor = sqrt(2), verbose = false, options...)
         finished=false
         USV = ()
+        R = min(R, size(op,2))
         random_matrix = map(ELT, rand(size(op,2), R))
         C = apply_multiple(op, random_matrix)
-        while R<=size(op,2)
+        c = cond(C)
+        m = maximum(abs(C))
+        while (c < m/cutoff) && (R<size(op,2))
             R0 = R
-            R = min(round(Int,sqrt(2)*R),size(op,2))
+            R = min(round(Int,growth_factor*R),size(op,2))
             extra_random_matrix = map(ELT, rand(size(op,2), R-R0))
             Cextra = apply_multiple(op, extra_random_matrix)
             random_matrix = [random_matrix extra_random_matrix]
             C = [C Cextra]
             c = cond(C)
             m = maximum(abs(C))
-            if (c>m/cutoff) || R==size(op,2)
-                USV = LAPACK.gesdd!('S',C)
-                S = USV[2]
-                maxind = findlast(S.>cutoff)
-                Sinv = 1./S[1:maxind]
-                y = zeros(ELT, size(USV[3],1))
-                sy = zeros(ELT, maxind)
-                Wsrc = ELT <: Complex ? Cn{ELT}(size(random_matrix,2)) : Rn{ELT}(size(random_matrix,2))
-                Wdest = src(op)
-                W = MatrixOperator(Wsrc, Wdest, random_matrix)            
-
-                return new(op, W, USV[1][:,1:maxind]',USV[3][1:maxind,:]'*diagm(Sinv[:]),y,sy)
-            end
         end
+        USV = LAPACK.gesdd!('S',C)
+        S = USV[2]
+        maxind = findlast(S.>cutoff)
+        Sinv = 1./S[1:maxind]
+        y = zeros(ELT, size(USV[3],1))
+        sy = zeros(ELT, maxind)
+        Wsrc = ELT <: Complex ? Cn{ELT}(size(random_matrix,2)) : Rn{ELT}(size(random_matrix,2))
+        Wdest = src(op)
+        W = MatrixOperator(Wsrc, Wdest, random_matrix)
+
+        scratch_src = zeros(ELT, length(dest(op)))
+        scratch_dest = zeros(ELT, length(src(op)))
+        new(op, W, USV[1][:,1:maxind]',USV[3][1:maxind,:]'*diagm(Sinv[:]),y,sy, scratch_src, scratch_dest)
     end
 end
 
@@ -56,9 +61,18 @@ inv(t::TruncatedSvdSolver) = t.op
 TruncatedSvdSolver(op::AbstractOperator; options...) =
     TruncatedSvdSolver{eltype(op)}(op::AbstractOperator; options...)
 
-function apply!(s::TruncatedSvdSolver, destset, srcset, coef_dest, coef_src)
+# We don't need to (de)linearize coefficients when they are already vectors
+function apply!(s::TruncatedSvdSolver, coef_dest::Vector, coef_src::Vector)
     # Applying the truncated SVD to P*Rhs
     A_mul_B!(s.sy, s.Ut, coef_src)
     A_mul_B!(s.y, s.VS, s.sy)
     apply!(s.W, coef_dest, s.y)
+end
+
+function apply!(s::TruncatedSvdSolver, coef_dest, coef_src)
+    linearize_coefficients!(src(s), s.scratch_src, coef_src)
+    A_mul_B!(s.sy, s.Ut, s.scratch_src)
+    A_mul_B!(s.y, s.VS, s.sy)
+    apply!(s.W, s.scratch_dest, s.y)
+    delinearize_coefficients!(dest(s), coef_dest, s.scratch_dest)
 end
