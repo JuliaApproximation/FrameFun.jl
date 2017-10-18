a5cd72198caa255d0db8c2c955706c0c5190e169
vincentcp <vincent.coppe@gmail.com>
2017-10-18 16:46:29 +0200

    REQUIRE Interact, Reactive

--- test/test_plots.jl
+++ test/test_plots.jl
@@ -8,61 +8,75 @@ using Plots
 using Base.Test
 FE = FrameFun
 BA = BasisFunctions
-gr()
-# test plotting functionsets
-B = ChebyshevBasis(5)
-plot(B)
 
-plot(B[4])
+function delimit(s::AbstractString)
+    println()
+    println("############")
+    println("# ",s)
+    println("############")
+end
 
-F = FourierBasis(5)
-plot(F[2],plot_complex=true)
+delimit("Plotting")
 
-B = ChebyshevBasis(5,-2,0)⊕FourierBasis(5,0,2)
-plot(B[2:8],plot_complex=true)
+function test_plots()
+    gr()
+    # test plotting functionsets
+    B = ChebyshevBasis(5)
+    plot(B)
 
-F = FourierBasis(10)⊗FourierBasis(10)
-plot(F[82],plot_complex=false)
+    plot(B[4])
 
-G = BasisFunctions.grid(ChebyshevBasis(51))
-plot(G)
+    F = FourierBasis(5)
+    plot(F[2],plot_complex=true)
 
-G = BasisFunctions.grid(ChebyshevBasis(51,-1,0.3)⊗ChebyshevBasis(51,-0.5,0.5))
-G = FrameFun.subgrid(G,mandelbrot())
-plot(G)
+    B = ChebyshevBasis(5,-2,0)⊕FourierBasis(5,0,2)
+    plot(B[2:8],plot_complex=true)
 
-B = FourierBasis(21,-1,1)⊗FourierBasis(21,-1,1)⊗FourierBasis(21,-1,1)
-D = ball()\FrameFun.cube(-0.5,0.5,-0.5,0.5,-2.,2.)
-Df = ExtensionFrame(D,B)
-G = BasisFunctions.grid(Df)
-plot(G,size=(400,400))
+    F = FourierBasis(10)⊗FourierBasis(10)
+    plot(F[82],plot_complex=false)
 
-B = FourierBasis(51,-2.0,-0.5)
-D = interval(-1.7,-1.0)
-f(x) = cos(3*x.^2)
-F = Fun(f,B,D)
-# Easily combine multiple plots
-plot(BasisFunctions.grid(set(F)),label="grid",markercolor=:white)
-plot!(F,label="function", plot_ext=true)
-plot!(F',title="Function and derivative",label="derivative",legend=true)
+    G = BasisFunctions.grid(ChebyshevBasis(51))
+    plot(G)
 
-plot(F,f,label="function")
-df(x) = -sin(3*x^2)*6*x
-plot!(F',df,label="derivative",legend=true)
+    G = BasisFunctions.grid(ChebyshevBasis(51,-1,0.3)⊗ChebyshevBasis(51,-0.5,0.5))
+    G = FrameFun.subgrid(G,mandelbrot())
+    plot(G)
 
-B = FourierBasis(21,-1,1)⊗FourierBasis(21,-1,1)
-D = intersect(disk(0.8),disk(0.4))
-f(x,y) = cos(7*x-2*y^2)
-F = Fun(f,B,D)
+    B = FourierBasis(21,-1,1)⊗FourierBasis(21,-1,1)⊗FourierBasis(21,-1,1)
+    D = ball()\FrameFun.cube(-0.5,0.5,-0.5,0.5,-2.,2.)
+    Df = ExtensionFrame(D,B)
+    G = BasisFunctions.grid(Df)
+    plot(G,size=(400,400))
 
-plot(D,n=100)
+    B = FourierBasis(51,-2.0,-0.5)
+    D = interval(-1.7,-1.0)
+    f(x) = cos(3*x.^2)
+    F = Fun(f,B,D)
+    # Easily combine multiple plots
+    plot(BasisFunctions.grid(set(F)),label="grid",markercolor=:white)
+    plot!(F,label="function", plot_ext=true)
+    plot!(F',title="Function and derivative",label="derivative",legend=true)
 
-plot(F,size=(600,400))
+    plot(F,f,label="function")
+    df(x) = -sin(3*x^2)*6*x
+    plot!(F',df,label="derivative",legend=true)
 
-contourf(F,colorbar=true)
+    B = FourierBasis(21,-1,1)⊗FourierBasis(21,-1,1)
+    D = intersect(disk(0.8),disk(0.4))
+    f(x,y) = cos(7*x-2*y^2)
+    F = Fun(f,B,D)
 
-plot(F,plot_ext=true)
+    plot(D,n=100)
 
-P = contour(F,f)
+    plot(F,size=(600,400))
+
+    contourf(F,colorbar=true)
+
+    plot(F,plot_ext=true)
+
+    P = contour(F,f)
+end
+
+test_plots()
 
 end
