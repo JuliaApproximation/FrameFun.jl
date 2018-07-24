1b2a3cefc0b139ae48c23d589dbd68185197e632
vincentcp <vincent.coppe@gmail.com>
2018-07-24 11:35:32 +0200

    rm deprecated code deps

--- deps/build.jl
+++ deps/build.jl
@@ -1,6 +1,7 @@
-
-Pkg.clone("https://github.com/vincentcp/LinearAlgebra.jl.git")
-Pkg.clone("https://github.com/daanhb/Domains.jl")
-Pkg.clone("https://github.com/daanhb/BasisFunctions.jl")
-Pkg.checkout("BasisFunctions", "julia-0.7")
-Pkg.build("BasisFunctions")
+if VERSION < v"0.7-"
+    Pkg.clone("https://github.com/daanhb/BasisFunctions.jl")
+    Pkg.checkout("BasisFunctions", "julia-0.7")
+    Pkg.build("BasisFunctions")
+else
+    Pkg.add("https://github.com/daanhb/BasisFunctions.jl#julia-0.7")
+end
