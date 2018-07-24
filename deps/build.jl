7e14c6b11ae0e3923d3906844e5d2e91a5cfffdb
vincentcp <vincent.coppe@gmail.com>
2018-07-24 09:24:53 +0200

    update desp

--- deps/build.jl
+++ deps/build.jl
@@ -1,4 +1,4 @@
-(VERSION < v"0.7-") ? nothing : Pkg.develop("https://github.com/MikaelSlevinsky/FastTransforms.jl")
+
 Pkg.clone("https://github.com/vincentcp/LinearAlgebra.jl.git")
 Pkg.clone("https://github.com/daanhb/Domains.jl")
 Pkg.clone("https://github.com/daanhb/BasisFunctions.jl")
