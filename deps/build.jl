07e3a84d863bbfb143fc35ccdc0a84885d5a8b89
vincentcp <vincent.coppe@gmail.com>
2018-07-24 12:45:18 +0200

    remove depr code from deps

--- deps/build.jl
+++ deps/build.jl
@@ -2,6 +2,4 @@ if VERSION < v"0.7-"
     Pkg.clone("https://github.com/daanhb/BasisFunctions.jl")
     Pkg.checkout("BasisFunctions", "julia-0.7")
     Pkg.build("BasisFunctions")
-else
-    Pkg.add("https://github.com/daanhb/BasisFunctions.jl#julia-0.7")
 end
