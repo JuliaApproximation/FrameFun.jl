1e6322f42724f2a9ca089e84082057a6c9e8847a
vincentcp <vincent.coppe@gmail.com>
2018-07-26 21:59:37 +0200

    update co to development

--- deps/build.jl
+++ deps/build.jl
@@ -1,6 +1,6 @@
 if VERSION < v"0.7-"
     Pkg.clone("https://github.com/daanhb/BasisFunctions.jl")
-    Pkg.checkout("BasisFunctions", "julia-0.7")
+    Pkg.checkout("BasisFunctions", "development")
     Pkg.build("BasisFunctions")
 end
 run(`rm -f '~/.jupyter/jupyter_nbconvert_config.json'`)
