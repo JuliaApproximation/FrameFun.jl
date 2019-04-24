ef1d0ab7bf685700df939c6c83174ddb9c9138d8
vincentcp <vincent.coppe@gmail.com>
2019-04-24 23:43:53 +0200

    bug fix build.jl

--- deps/build.jl
+++ deps/build.jl
@@ -1,5 +1,5 @@
 
 run(`rm -f '~/.jupyter/jupyter_nbconvert_config.json'`)
 using Pkg
-Pkg.instantiate(splitdir(@__DIR__())[1])
+Pkg.activate(splitdir(@__DIR__)[1])
 pkg"add https://github.com/daanhb/BasisFunctions.jl"
