33e58e0b62b9456ff3bed9b17c6cebb1afdfd8ae
Daan Huybrechs <daan.huybrechs@cs.kuleuven.be>
2019-11-27 18:09:02 +0100

    interchange type arguments of Vararg

--- src/generic/Platforms/ParameterPaths/ParameterPaths.jl
+++ src/generic/Platforms/ParameterPaths/ParameterPaths.jl
@@ -88,8 +88,8 @@ param_increment(path::CartesianParameterPath{N}, param::NTuple{N}) where N =
 
 unsafe_getindex(path::CartesianParameterPath{N}, index::Real) where N =
     round.(Int, (index^(1/N)).*path.constant)
-unsafe_getindex(path::CartesianParameterPath{N}, index::Vararg{N,<:Real}) where N =
-    unsafe_getindex(path, index)
+unsafe_getindex(path::CartesianParameterPath{N}, index::Vararg{<:Real,N}) where N =
+   unsafe_getindex(path, index)
 unsafe_getindex(path::CartesianParameterPath{N}, index::CartesianIndex{N}) where N =
     unsafe_getindex(path, index.I)
 unsafe_getindex(path::CartesianParameterPath{N}, index::NTuple{N,<:Real}) where N =
@@ -97,7 +97,7 @@ unsafe_getindex(path::CartesianParameterPath{N}, index::NTuple{N,<:Real}) where 
 correctparamformat(::CartesianParameterPath, ::Real) = true
 correctparamformat(::CartesianParameterPath{N}, ::NTuple{N,<:Real}) where N = true
 correctparamformat(::CartesianParameterPath{N}, ::CartesianIndex{N}) where N = true
-correctparamformat(::CartesianParameterPath{N}, ::Vararg{N,<:Real}) where N = true
+correctparamformat(::CartesianParameterPath{N}, ::Vararg{<:Real,N}) where N = true
 
 
 export IncrementalCartesianParameterPath
